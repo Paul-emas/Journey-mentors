146403b58db554cee8e39c3494bd604611756253
Paul Emas <pjobiggi@gmail.com>
2026-07-12 09:37:01 +0100

    feat(search): wire flight search composable to proxy

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,23 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
+import { useFlightSearch } from '@/composables/useFlightSearch.ts';
 import TheWelcome from '../components/TheWelcome.vue'
+
+const { search } = useFlightSearch()
+
+onMounted(() => {
+  search({
+    origin: 'LHR',
+    destination: 'JFK',
+    departureDate: '2026-08-14',
+    cabinClass: 'economy',
+    passengers: {
+      adults: 1,
+      children: 0,
+      infants: 0,
+    },
+  })
+})
 </script>
 
 <template>
